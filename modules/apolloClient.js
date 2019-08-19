ffe149a5c3e568ccb810d321cd75be4c0b8596df
hugohammarstrom <hugo.hammarstrom@icloud.com>
2019-08-19 14:56:22 +0200

    use live api

--- modules/apolloClient.js
+++ modules/apolloClient.js
@@ -8,8 +8,8 @@ export const client = new ApolloClient({
   cache: new InMemoryCache(),
   connectToDevTools: process.browse,
   link: createHttpLink({
-      // uri: "https://api.wdnsolutions.com",
-      uri: "http://localhost:4000",
+      uri: "https://api.wdnsolutions.com",
+      // uri: "http://localhost:4000",
       fetch: process.browser ? window.fetch : fetch
   })
 })
